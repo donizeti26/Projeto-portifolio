aaee9807d287c9c6dac3a4c75e556d3a560958a6
donizeti_silva <61122214+donizeti26@users.noreply.github.com>
2024-04-19 13:40:58 -0300

    atualizando icons iniciar

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var area = window.document.getElementsByClassName("dropdown")[0]
 
-
 var item = window.document.getElementById("contact")
+
 
 
 area.addEventListener('mouseenter', colorAdd, false)
@@ -16,4 +16,28 @@ function colorAdd(){
 function colorRemove(){
     item.style.color = "var(--text-color-grey)"
     
+}
+
+
+
+var toggle= window.document.getElementById("checkbox")
+toggle.addEventListener('click', toggleMode)
+
+
+function toggleMode(){
+    if(toggle.checked){
+        document.body.setAttribute('class', 'shadow')
+    }else{
+        document.body.setAttribute('class', 'light')
+
+    }
+}
+
+
+
+var english= window.document.getElementById("language-usa")
+english.addEventListener('click', openenglish)
+
+function openenglish(){
+    console.log("OK!")
 }
